b8564dfec88095db8ad8cc982fd721c98f11a15c
Ishika Mukerji <ishikamukerji@gmail.com>
2023-12-22 16:55:00 -0500

    Update app.py

--- examples/sadhguru-ai/app.py
+++ examples/sadhguru-ai/app.py
@@ -40,10 +40,10 @@ def add_data_to_app():
 app = sadhguru_ai()
 add_data_to_app()
 
-assistant_avatar_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Sadhguru-Jaggi-Vasudev.jpg/640px-Sadhguru-Jaggi-Vasudev.jpg"  # noqa: E501
+assistant_avatar_url = "https://kfoundation.org/wp-content/uploads/2022/08/Jiddu_Krishnamurti.jpg"  # noqa: E501
 
 
-st.title("🙏 Sadhguru AI")
+st.title("🙏 J. Krishnamurti AI")
 
 styled_caption = '<p style="font-size: 17px; color: #aaa;">🚀 An <a href="https://github.com/embedchain/embedchain">Embedchain</a> app powered with Sadhguru\'s wisdom!</p>'  # noqa: E501
 st.markdown(styled_caption, unsafe_allow_html=True)  # noqa: E501
@@ -53,7 +53,11 @@ if "messages" not in st.session_state:
         {
             "role": "assistant",
             "content": """
-                Hi, I'm Sadhguru AI! I'm a mystic, yogi, visionary, and spiritual master. I'm here to answer your questions about life, the universe, and everything.
+                Krishnamurti is regarded globally as one of the greatest thinkers and religious teachers of all time. He did not expound any philosophy or religion, but rather talked of the things that concern all of us in our everyday lives, of the problems of living in modern society with its violence and corruption, of the individual's search for security and happiness, and the need for mankind to free itself from inner burdens of fear, anger, hurt, and sorrow. He explained with great precision the subtle workings of the human mind, and pointed to the need for bringing to our daily life a deeply meditative and spiritual quality.
+
+Krishnamurti belonged to no religious organization, sect or country, nor did he subscribe to any school of political or ideological thought. On the contrary, he maintained that these are the very factors that divide human beings and bring about conflict and war. He reminded his listeners again and again that we are all human beings first and not Hindus, Muslims or Christians, that we are like the rest of humanity and are not different from one another. He asked that we tread lightly on this earth without destroying ourselves or the environment. He communicated to his listeners a deep sense of respect for nature. His teachings transcend man-made belief systems, nationalistic sentiment and sectarianism. At the same time, they give new meaning and direction to mankind's search for truth. His teaching, besides being relevant to the modern age, is timeless and universal.
+
+Krishnamurti spoke not as a guru but as a friend, and his talks and discussions are based not on tradition-based knowledge but on his own insights into the human mind and his vision of the sacred, so he always communicates a sense of freshness and directness although the essence of his message remained unchanged over the years. When he addressed large audiences, people felt that Krishnamurti was talking to each of them personally, addressing his or her particular problem. In his private interviews, he was a compassionate teacher, listening attentively to the man or woman who came to him in sorrow, and encouraging them to heal themselves through their own understanding. Religious scholars found that his words threw new light on traditional concepts. Krishnamurti took on the challenge of modern scientists and psychologists and went with them step by step, discussed their theories and sometimes enabled them to discern the limitations of those theories. Krishnamurti left a large body of literature in the form of public talks, writings, discussions with teachers and students, with scientists and religious figures, conversations with individuals, television and radio interviews, and letters. Many of these have been published as books, and audio and video recordings.
             """,  # noqa: E501
         }
     ]
