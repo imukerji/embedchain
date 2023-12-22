281eeaef8ca05d405cd5d509d8785b7aa2331bba
Ishika Mukerji <ishikamukerji@gmail.com>
2023-12-22 09:16:37 -0500

    Update app.py

--- examples/sadhguru-ai/app.py
+++ examples/sadhguru-ai/app.py
@@ -29,7 +29,7 @@ def read_csv_row_by_row(file_path):
 @st.cache_resource
 def add_data_to_app():
     app = sadhguru_ai()
-    url = "https://gist.githubusercontent.com/deshraj/50b0597157e04829bbbb7bc418be6ccb/raw/95b0f1547028c39691f5c7db04d362baa597f3f4/data.csv"  # noqa:E501
+    url = "https://gist.githubusercontent.com/imukerji/58bafd0a7d655dbb47d9a158a237fb49/raw/75bc527eee95b83ca0427dd8ef6e2cdcf12cec24/gistfile1.csv"  # noqa:E501
     response = requests.get(url)
     csv_file = StringIO(response.text)
     for row in csv.reader(csv_file):
